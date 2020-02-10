10640aa5042d3cad021db9dc4ec18e629ae6b062
Sinclair Chen <abc.sinclair@gmail.com>
2020-02-09 20:37:14 -0800

    stash

--- projects/siblock/game.js
+++ projects/siblock/game.js
@@ -1,48 +1,153 @@
 "use strict";
-//centering game window
-// var pageW = window.innerWidth; //webpage width
-// var pageH = window.innerHeight; //webpage height
-var gWin = $("#gameWin");
-// var winW = gWin.width();
-// var winH = gWin.height();
-// var winTop = gWin.offset().top; //dist from top to game window
-// var winLeft = gWin.offset().left;
+
+var gWin = document.getElementById("mainWindow");
+//match css width (and zoom I guess)
+gWin.width = gWin.clientWidth;
+gWin.height = gWin.clientHeight; 
 
 //get the drawable part of the game window
-var cx = gWin[0].getContext("2d");
-// usage:
-// cx.fillStyle = "brown";
-// cx.fillRect(this.x, this.y, this.width, this.height);
+var cx = gWin.getContext("2d");
 
-var gameBoard = {
-	//10x10 array
-	cells: new Array(10).fill(0).map(() => new Array(10).fill(0 /*TODO replace with empty cells*/)),
-	draw() {
-		cx.fillStyle = "brown";
-		cx.fillRect(20, 20, 100, 100);
-		//iterate through cells and draw them
+// TODO this is far off
+class Tile {
+	constructor(src) {
+		this.image = new Image(20, 20)
+			.src = src;
 	}
 }
-console.log(gameBoard);
 
 //TODO svgs
+const minoWidth = 20; //TODO relative
+
+var empty = new Image(minoWidth, minoWidth);
+empty.src = "assets/empty.svg";
+
+function translate(x, y) {
+	return (rel_x, rel_y) => [x + rel_x, y + rel_y];
+}
+
+var well = new (class Well {
+	constructor() {
+		this.width = 10;
+		this.depth = 20;
+		this.cells = new Array(this.depth).fill(0)
+			.map(() => new Array(this.width).fill(empty));
+	}
+	draw() {
+		let xy = translate(
+			gWin.width / 2 - minoWidth * this.width / 2, //center
+			gWin.height * 0.2
+		);
+		//draw boundry
+		cx.strokeStyle = "black"
+		cx.rect(...xy(0, 0), this.width * minoWidth, this.depth * minoWidth);
+		cx.fillStyle = cx.createPattern(empty, "repeat");
+		cx.fill();
+		cx.stroke();
+		//iterate through cells and draw them
+		cx.drawImage(empty, 300, 300);
+	}
+})()
+console.log(well);
+
+var palletteTest = {
+	draw() {
+		for (let piece in pieces) {
+			cx.fillStyle = pieces[piece].color;
+			cx.fillRect(Math.random()*400, Math.random()*400, 100, 100);
+		}
+	}
+}
+
 
 //TODO core game loop
-
-//10x10 array of cells
 
 //place shadow
 
 //drop shadow
 
+/* Piece and rotation logic. See https://tetris.wiki/Super_Rotation_System */
+//TODO reverse all y's
+
+//jLszt
+var kick3wide = {
+	U: Array(4).fill((0, 0)),
+	R: [[-1, 0], [-1, +1], [0, -2], [-1, -2]],
+	//D is same as U
+	L: [[+1, 0], [+1, +1], [0, -2], [+1, -2]],
+}
+//i
+var kick4wide = {
+	U: Array(4).fill((0, 0)),
+	R: [[-2, 0], [+1, 0], [-2, -1], [+1, +2]],
+	D: [[-3, 0], [+3, 0], [-3, +1], [+3, -1]],
+	L: [[-1, 0], [+2, 0], [-1, +2], [+2, -1]],
+}
+
 var pieces = {
-	j: {color: "", kick: () => {}},
-	i: {color: "", kick: () => {}},
-	z: {color: "", kick: () => {}},
-	L: {color: "", kick: () => {}},
-	o: {color: "", kick: () => {}},
-	t: {color: "", kick: () => {}},
-	s: {color: "", kick: () => {}},
+	j: {
+		color: "dodgerblue",
+		start:
+		[
+			[0, 0, 0],
+			[1, 1, 1],
+			[0, 0, 1],
+		]
+	},
+	i: {
+		color: "aqua",
+		start:
+		[
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+			[1, 1, 1, 1],
+			[0, 0, 0, 0],
+		]
+	},
+	z: {
+		color: "red",
+		start:
+		[
+			[1, 1, 0],
+			[0, 1, 1],
+			[0, 0, 0],
+		]
+	},
+	L: {
+		color: "orange",
+		start:
+		[
+			[0, 0, 1],
+			[1, 1, 1],
+			[0, 0, 0],
+		]
+	},
+	o: {
+		color: "yellow",
+		start:
+		[
+			[1, 1],
+			[1, 1],
+		]
+	},
+	t: {
+		color: "magenta",
+		start:
+		[
+			[0, 1, 0],
+			[1, 1, 1],
+			[0, 0, 0],
+		]
+	},
+	s: {
+		color: "limegreen",
+		start:
+		[
+			[0, 1, 1],
+			[1, 1, 0],
+			[0, 0, 0],
+		]
+	},
 }
 
 //kick tables)
@@ -60,45 +165,41 @@ var mover = {
 // TODO replace all of this with on drag and drop events lmao
 
 // TODO custom keybindings
-var keycodes = {
-	w: 87,
-	a: 65,
-	s: 83,
-	d: 68,
-	left: 37,
-	up: 38,
-	right: 39,
-	down: 40,
-}
-// var code to keyname is reverse map
 
+var paused = false;
 // TODO fix
 // document.body.onKeyDown = onDown;
 // document.body.onKeyUp = onUp;
-$("body").keydown(onDown);
-$("body").keyup(onUp);
+document.addEventListener("keydown", onDown)
+document.addEventListener("keyup", onUp)
 //when key is pressed, change the movement to true
 function onDown(event)
 {
-	console.log("down: " + event.keyCode);
-	mover.going[event.keyCode] = true
+	console.log("down: " + event.code);
+	mover.going[event.code] = true
 }
 
 //undo the keypress
 function onUp(event)
 {
-	console.log("up: " + event.keyCode);
-	mover.going[event.keyCode] = false
+	console.log("up: " + event.code);
+	mover.going[event.code] = false
+	if (event.code == "KeyP"){
+		paused = !paused;
+	}
 }
 
 function updateImage()
 {
-	//draw room
-	gameBoard.draw()
-	if (mover.draw) {
-		mover.draw();
+	if (paused) {
+		return;
 	}
+	//draw room
+	palletteTest.draw();
+	well.draw();
+	mover.draw();
 }
 
 //60 fps is about 16 ms / frame 
-setInterval(updateImage, 16);
+//setInterval(updateImage, 16);
+updateImage()
